a05d69dfabad339456b9fd0820d45053ad538ac8
GigihHaidar <gigihhaidar15@gmail.com>
2024-10-23 06:15:45 +0700

    3number

---
+++ basicPython/numb3.py
@@ -0,0 +1,9 @@
+# Input dua bilangan bulat
+a = int(input())
+b = int(input())
+
+# Mencetak hasil pembagian bilangan bulat (integer division)
+print(a // b)
+
+# Mencetak hasil pembagian bilangan desimal (float division)
+print(a / b)
