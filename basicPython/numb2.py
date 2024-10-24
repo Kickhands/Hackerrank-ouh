66698d7198af3e4f6528b57f4b4d5f05682b20b9
GigihHaidar <gigihhaidar15@gmail.com>
2024-10-24 11:31:25 +0700

    numb6-7

---
+++ basicPython/numb2.py
@@ -0,0 +1,12 @@
+# Input dua bilangan bulat
+a = int(input())
+b = int(input())
+
+# Mencetak jumlah
+print(a + b)
+
+# Mencetak selisih (a - b)
+print(a - b)
+
+# Mencetak hasil kali
+print(a * b)
---
+++ basicPython/numb6.py
@@ -0,0 +1,6 @@
+# Input: Read an integer n
+n = int(input())
+
+# Output: Print integers from 1 to n without spaces
+for i in range(1, n + 1):
+    print(i, end="")
